524f8a4fdc19c7e7c19df467829d47a340796889
Rory Wood <rorywood9@live.com>
2024-01-20 13:03:14 -0500

    updated bash scripts

--- scripts/startUpdate.sh
+++ scripts/startUpdate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup php /home/raspberrypi/programs/updateDisplay/todoList/getHTML.php > /dev/null 2>&1 &
+nohup php /home/pi/programs/updateDisplay/getHTML.php > /dev/null 2>&1 &
--- scripts/updateDisplay.sh
+++ scripts/updateDisplay.sh
@@ -5,6 +5,6 @@ filename=todolist.html
 
 firefox --headless --screenshot --window-size=800,480 file://$CWD/$filename
 
-python3 displayImage.py screenshot.png 
+python3 ./scripts/displayImage.py screenshot.png 
 
 rm screenshot.png 
